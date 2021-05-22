08de5af8abbe17819eec1df0b301ce391cfa7749
DeltaF1 <delyaerricson@gmail.com>
2021-05-22 01:50:30 -0400

    Initial Commit

---
+++ extras/example6809.fs
@@ -0,0 +1,12 @@
+( Usage: cos-serial < example6809.fs )
+50 LOAD ( 6809 asm )
+470 486 LOADR
+0x40 CONSTANT SZ
+SZ allotmem
+SZ S REG T!
+MEM @ H !
+0x0804 # LDD, MUL, SYNC,
+16 MEM @ DUMP
+run
+cpudump
+SZ MEM @ DUMP
